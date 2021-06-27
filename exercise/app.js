a3bd47053194b9b216146c3e1b2e435af863d054
sanjay <sanssitk@gmail.com>
2021-06-27 00:07:42 -0500

    pulled query search from twitter api

--- exercise/app.js
+++ exercise/app.js
@@ -1,11 +1,37 @@
-const express = require("express")
+const express = require("express");
 const app = express();
+const axios = require("axios");
+require("dotenv").config();
 
-app.get("/", (req, res) => {
-    res.send("Hello World")
-})
+const apiKey = process.env.TWIITER_APP_API_KEYS;
+const apiSecretKehy = process.env.TWIITER_APP_API_SECRET_KEY;
+const bearerToken = process.env.BEARER_TOKEN;
 
+app.get("/tweets", (req, res) => {
+  const query = req.query.query;
+  console.log(query);
+  const max_results = req.query.max_results;
+  const url = "https://api.twitter.com/2/tweets/search/recent";
+  axios
+    .get(url, {
+      params: {
+        query: query,
+        max_results: max_results,
+      },
+      headers: {
+        Authorization: `Bearer ${bearerToken}`,
+      },
+    })
+    .then((res) => {
+      res.status(200).send(res.data);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+
+  res.send("Hello World");
+});
 
 app.listen(3000, () => {
-    console.log("Listening on localhost:3000")
-})
+  console.log("Listening on localhost:3000");
+});
