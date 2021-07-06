fb8c254164673839e36829a1607011f1a69c9f63
sanjay <sanssitk@gmail.com>
2021-07-05 21:41:33 -0500

    complete project

--- exercise/api/helpers/Twitters.js
+++ exercise/api/helpers/Twitters.js
@@ -1,25 +1,24 @@
 const axios = require("axios");
 require("dotenv").config();
 
-const URL = "https://api.twitter.com/2/tweets/search/recent";
-//   const urlall = "https://api.twitter.com/2/tweets/search/all";
-// https://api.twitter.com/2/tweets/search/recent?query=snow&max_results=50&tweet.fields=id,created_at,author_id,text,source,entities,attachments&user.fields=id,name,username,description
+const URL = "https://api.twitter.com/1.1/search/tweets.json";
 const bearerToken = process.env.BEARER_TOKEN;
 const apiKey = process.env.TWIITER_APP_API_KEYS;
 const apiSecretKehy = process.env.TWIITER_APP_API_SECRET_KEY;
 
 class Twitter {
-  get(query, max_results) {
-    return axios
-      .get(URL, {
-        params: {
-          query: query,
-          max_results: max_results,
-        },
-        headers: {
-          Authorization: `Bearer ${bearerToken}`,
-        },
-      })
+  get(q, count, maxId) {
+    return axios.get(URL, {
+      params: {
+        q: q,
+        count: count,
+        tweet_mode: "extended",
+        max_id: maxId
+      },
+      headers: {
+        Authorization: `Bearer ${bearerToken}`,
+      },
+    });
   }
 }
 
--- exercise/app.js
+++ exercise/app.js
@@ -1,24 +1,34 @@
 const express = require("express");
 const app = express();
+const port = process.env.port || 3000 ;
+
+// middleware to convert json to javascript objects
+app.use(express.json());
+
+// Middleware to allow access-control
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
 
 const Twitter = require("./api/helpers/Twitters");
 const twitter = new Twitter();
 
 app.get("/tweets", (req, res) => {
-  const query = req.query.query;
-  const max_results = req.query.max_results;
+  const q = req.query.q;
+  const count = req.query.count;
+  const maxId = req.query.max_id;
 
   twitter
-    .get(query, max_results)
+    .get(q, count, maxId)
     .then((response) => {
       res.status(200).send(response.data);
     })
-
     .catch((err) => {
       res.status(400).send(err);
     });
 });
 
-app.listen(3000, () => {
-  console.log("Listening on localhost:3000");
+app.listen(port, () => {
+  console.log(`Listening on localhost:${port}`);
 });
